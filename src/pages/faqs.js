879bf635fc542731c177d6ef6e6a3d92f2fcfb26
Jayashree-panda <jayashreepanda12345@gmail.com>
2019-05-17 15:14:53 +0530

    faq page

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -1,105 +1,211 @@
 import React from "react";
-import Link from "gatsby-link";
+import PropTypes from "prop-types";
 import NavigationBar from "../components/Navigationbar";
-
-const outerTypo = {
-  fontFamily: "Roboto",
-  fontSize: `28px`,
-  marginLeft: "20%"
-};
-
-const innerTypo = {
-  fontFamily: "Roboto",
-  fontSize: `20px`,
-  backgroundColor: "#EAEDED",
-  borderRadius: "20px",
-  textAlign: "left",
-  maxWidth: `57%`,
-  marginLeft: "20%"
-};
-
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Zoom from "@material-ui/core/Zoom";
+import Slide from "@material-ui/core/Slide";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const styles = theme => ({
-  root: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2
+  paper: {
+    backgroundColor: "#37474f",
+    maxWidth: "70%",
+    margin: "auto",
+    padding: "10% 5%"
+  },
+  typo: {
+    color: "#ffebee",
+    fontSize: "225%"
+  },
+  exp: {
+    maxWidth: "70%",
+    margin: "auto"
+  },
+  heading: {
+    fontSize: "100%"
+  },
+  typ: {
+    fontSize: "100%"
+  },
+  expsum: {
+    border: "none",
+    padding: "1%"
   }
 });
 
-const FAQ = props => {
+function Faq(props) {
   const { classes } = props;
 
   return (
     <div>
-      <div>
-        <NavigationBar />
-        <h1 align="center">FAQs</h1>
-      </div>
+      <NavigationBar />
+      <Zoom in="false">
+        <Paper className={classes.paper}>
+          <Typography variant="h5" component="h3" className={classes.typo}>
+            Following are the most frequently asked questions about zairza
+          </Typography>
+        </Paper>
+      </Zoom>
 
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h5" gutterBottom style={outerTypo}>
-          Why Zairza?
-        </Typography>
-        <Typography component="p" gutterBottom style={innerTypo}>
-          For people persuing careers in computers and/or robotics, writing
-          code, developing softwares, and creating exceptional things. That's
-          what we do here. We do things and we help people who are really
-          interested into them.
-        </Typography>
-      </Paper>
+      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
 
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h3" gutterBottom style={outerTypo}>
-          How can I join?
-        </Typography>
-        <Typography component="p" gutterBottom style={innerTypo}>
-          Zairza conducts its formal events for recruitment into the club. The
-          pre-requisite includes passion for technology and an inquisitive mind.
-          Please contact the club administration for the exact procudures.
-        </Typography>
-      </Paper>
+      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>
+              I have a Tech project. How can Zairza help me?
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
 
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h3" gutterBottom style={outerTypo}>
-          What is the scope of Zairza?
-        </Typography>
-        <Typography component="p" gutterBottom style={innerTypo}>
-          The talent pool of the club is widely ranged. We have members working
-          on Web Development, Android Development, Embedded Systems, IOT,
-          Robotics, Machine Learning, Computer Vision and Artificial
-          Intelligence. Our Alumnis are placed in prestigious organization
-          around the globe.
-        </Typography>
-      </Paper>
+      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
 
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h3" gutterBottom style={outerTypo}>
-          I have a Tech project. How can Zairza help me?
-        </Typography>
-        <Typography component="p" gutterBottom style={innerTypo}>
-          Zairza can provide you with space and environment to work on your
-          project. We have stable fast internet connection and all the tools,
-          components and other required resources for your project. Given proper
-          report, we can also fund your project. Apart from this, we have senior
-          experienced mentors who will be more than happy to guide you
-          throughout your project.
-        </Typography>
-      </Paper>
+      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
 
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h3" gutterBottom style={outerTypo}>
-          What is the scope of Zairza?
-        </Typography>
-        <Typography component="p" gutterBottom style={innerTypo}>
-          The talent pool of the club is widely ranged. We have members working
-          on Web Development, Android Development, Embedded Systems, IOT,
-          Robotics, Machine Learning, Computer Vision and Artificial
-          Intelligence. Our Alumnis are placed in prestigious organization
-          around the globe.
-        </Typography>
-      </Paper>
+      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            className={classes.expsum}
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.typ}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
     </div>
   );
-};
+}
 
-export default withStyles(styles)(FAQ);
+Faq.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(Faq);
