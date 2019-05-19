90c9b86eba12aada1afdf2f3a41286c90fcc53f2
Jayashree-panda <jayashreepanda12345@gmail.com>
2019-05-19 16:11:10 +0530

    FAQ

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -1,66 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
 import NavigationBar from "../components/Navigationbar";
-import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+import {
+  withStyles,
+  MuiThemeProvider,
+  createMuiTheme
+} from "@material-ui/core/styles";
+import classNames from "classnames";
 import Typography from "@material-ui/core/Typography";
-import Zoom from "@material-ui/core/Zoom";
-import Slide from "@material-ui/core/Slide";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Slide from "@material-ui/core/Slide";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const styles = theme => ({
-  paper: {
-    backgroundColor: "#37474f",
-    maxWidth: "70%",
-    margin: "auto",
-    padding: "10% 5%"
-  },
-  typo: {
-    color: "#ffebee",
-    fontSize: "225%"
+  heading: {
+    fontSize: "110%"
   },
   exp: {
     maxWidth: "70%",
-    margin: "auto"
-  },
-  heading: {
-    fontSize: "100%"
-  },
-  typ: {
-    fontSize: "100%"
+    margin: "auto",
+    marginBottom: "0.7%"
   },
   expsum: {
-    border: "none",
-    padding: "1%"
+    padding: "2%"
+  },
+  details: {
+    fontSize: "100%"
   }
 });
-
 function Faq(props) {
   const { classes } = props;
-
   return (
     <div>
       <NavigationBar />
-      <Zoom in="false">
-        <Paper className={classes.paper}>
-          <Typography variant="h5" component="h3" className={classes.typo}>
-            Following are the most frequently asked questions about zairza
-          </Typography>
-        </Paper>
-      </Zoom>
-
-      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
+      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
         <ExpansionPanel className={classes.exp}>
           <ExpansionPanelSummary
-            className={classes.expsum}
             expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
           >
             <Typography className={classes.heading}>Why Zairza?</Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
+            <Typography className={classes.details}>
               For people persuing careers in computers and/or robotics, writing
               code, developing softwares, and creating exceptional things.
               That's what we do here. We do things and we help people who are
@@ -70,133 +53,185 @@ function Faq(props) {
         </ExpansionPanel>
       </Slide>
 
-      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
+      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
         <ExpansionPanel className={classes.exp}>
           <ExpansionPanelSummary
-            className={classes.expsum}
             expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
+          >
+            <Typography className={classes.heading}>How can I join?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.details}>
+              Zairza conducts its formal events for recruitment into the club.
+              The pre-requisite includes passion for technology and an
+              inquisitive mind. Please contact the club administration for the
+              exact procudures.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+
+      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
+          >
+            <Typography className={classes.heading}>
+              What is the scope of Zairza?
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.details}>
+              The talent pool of the club is widely ranged. We have members
+              working on Web Development, Android Development, Embedded Systems,
+              IOT, Robotics, Machine Learning, Computer Vision and Artificial
+              Intelligence. Our Alumnis are placed in prestigious organization
+              around the globe.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+
+      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
+          >
+            <Typography className={classes.heading}>Why Zairza?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.details}>
+              For people persuing careers in computers and/or robotics, writing
+              code, developing softwares, and creating exceptional things.
+              That's what we do here. We do things and we help people who are
+              really interested into them.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+
+      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
           >
             <Typography className={classes.heading}>
               I have a Tech project. How can Zairza help me?
             </Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            className={classes.expsum}
-            expandIcon={<ExpandMoreIcon />}
-          >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
+            <Typography className={classes.details}>
+              Zairza can provide you with space and environment to work on your
+              project. We have stable fast internet connection and all the
+              tools, components and other required resources for your project.
+              Given proper report, we can also fund your project. Apart from
+              this, we have senior experienced mentors who will be more than
+              happy to guide you throughout your project.
             </Typography>
           </ExpansionPanelDetails>
         </ExpansionPanel>
       </Slide>
 
-      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
+      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
         <ExpansionPanel className={classes.exp}>
           <ExpansionPanelSummary
-            className={classes.expsum}
             expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
           >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
+            <Typography className={classes.heading}>
+              What are the club timings?
+            </Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
+            <Typography className={classes.details}>
+              Zairza is open 24 hrs a day. Just coordinate with the members for
+              the key.
             </Typography>
           </ExpansionPanelDetails>
         </ExpansionPanel>
       </Slide>
-      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
+      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
         <ExpansionPanel className={classes.exp}>
           <ExpansionPanelSummary
-            className={classes.expsum}
             expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
           >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
+            <Typography className={classes.heading}>How can I join?</Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            className={classes.expsum}
-            expandIcon={<ExpandMoreIcon />}
-          >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-      <Slide direction="right" in="false" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            className={classes.expsum}
-            expandIcon={<ExpandMoreIcon />}
-          >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
+            <Typography className={classes.details}>
+              Zairza conducts its formal events for recruitment into the club.
+              The pre-requisite includes passion for technology and an
+              inquisitive mind. Please contact the club administration for the
+              exact procudures.
             </Typography>
           </ExpansionPanelDetails>
         </ExpansionPanel>
       </Slide>
 
-      <Slide direction="left" in="false" mountOnEnter unmountOnExit>
+      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
         <ExpansionPanel className={classes.exp}>
           <ExpansionPanelSummary
-            className={classes.expsum}
             expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
           >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
+            <Typography className={classes.heading}>
+              What is the scope of Zairza?
+            </Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography className={classes.typ}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
+            <Typography className={classes.details}>
+              The talent pool of the club is widely ranged. We have members
+              working on Web Development, Android Development, Embedded Systems,
+              IOT, Robotics, Machine Learning, Computer Vision and Artificial
+              Intelligence. Our Alumnis are placed in prestigious organization
+              around the globe.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+
+      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
+          >
+            <Typography className={classes.heading}>
+              I have a Tech project. How can Zairza help me?
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.details}>
+              Zairza can provide you with space and environment to work on your
+              project. We have stable fast internet connection and all the
+              tools, components and other required resources for your project.
+              Given proper report, we can also fund your project. Apart from
+              this, we have senior experienced mentors who will be more than
+              happy to guide you throughout your project.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Slide>
+
+      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
+        <ExpansionPanel className={classes.exp}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            className={classes.expsum}
+          >
+            <Typography className={classes.heading}>
+              What are the club timings?
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.details}>
+              Zairza is open 24 hrs a day. Just coordinate with the members for
+              the key.
             </Typography>
           </ExpansionPanelDetails>
         </ExpansionPanel>
@@ -208,4 +243,5 @@ function Faq(props) {
 Faq.propTypes = {
   classes: PropTypes.object.isRequired
 };
+
 export default withStyles(styles)(Faq);
