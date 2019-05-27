e390c5ef2d95dbf95855cc342d37d5d67bb75be7
Jayashree-panda <jayashreepanda12345@gmail.com>
2019-05-27 16:02:52 +0530

    used map function

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import NavigationBar from "../components/Navigationbar";
+import data from "../config/quest_ans.yaml";
 import {
   withStyles,
   MuiThemeProvider,
@@ -34,208 +35,26 @@ function Faq(props) {
   return (
     <div>
       <NavigationBar />
-      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
 
-      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>How can I join?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              Zairza conducts its formal events for recruitment into the club.
-              The pre-requisite includes passion for technology and an
-              inquisitive mind. Please contact the club administration for the
-              exact procudures.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>
-              What is the scope of Zairza?
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              The talent pool of the club is widely ranged. We have members
-              working on Web Development, Android Development, Embedded Systems,
-              IOT, Robotics, Machine Learning, Computer Vision and Artificial
-              Intelligence. Our Alumnis are placed in prestigious organization
-              around the globe.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>Why Zairza?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              For people persuing careers in computers and/or robotics, writing
-              code, developing softwares, and creating exceptional things.
-              That's what we do here. We do things and we help people who are
-              really interested into them.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>
-              I have a Tech project. How can Zairza help me?
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              Zairza can provide you with space and environment to work on your
-              project. We have stable fast internet connection and all the
-              tools, components and other required resources for your project.
-              Given proper report, we can also fund your project. Apart from
-              this, we have senior experienced mentors who will be more than
-              happy to guide you throughout your project.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>
-              What are the club timings?
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              Zairza is open 24 hrs a day. Just coordinate with the members for
-              the key.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>How can I join?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              Zairza conducts its formal events for recruitment into the club.
-              The pre-requisite includes passion for technology and an
-              inquisitive mind. Please contact the club administration for the
-              exact procudures.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>
-              What is the scope of Zairza?
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              The talent pool of the club is widely ranged. We have members
-              working on Web Development, Android Development, Embedded Systems,
-              IOT, Robotics, Machine Learning, Computer Vision and Artificial
-              Intelligence. Our Alumnis are placed in prestigious organization
-              around the globe.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="left" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>
-              I have a Tech project. How can Zairza help me?
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              Zairza can provide you with space and environment to work on your
-              project. We have stable fast internet connection and all the
-              tools, components and other required resources for your project.
-              Given proper report, we can also fund your project. Apart from
-              this, we have senior experienced mentors who will be more than
-              happy to guide you throughout your project.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
-
-      <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
-        <ExpansionPanel className={classes.exp}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            className={classes.expsum}
-          >
-            <Typography className={classes.heading}>
-              What are the club timings?
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className={classes.details}>
-              Zairza is open 24 hrs a day. Just coordinate with the members for
-              the key.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </Slide>
+      {Object.keys(data.faqs).map((key, id) => (
+        <Slide direction="right" in="flase" mountOnEnter unmountOnExit>
+          <ExpansionPanel className={classes.exp}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              className={classes.expsum}
+            >
+              <Typography className={classes.heading}>
+                {data.faqs[key].question}
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography className={classes.details}>
+                {data.faqs[key].answer}
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </Slide>
+      ))}
     </div>
   );
 }
