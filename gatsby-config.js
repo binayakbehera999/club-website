f025dd83d286ed24f7dd0482ea947b2913f21db5
Aquib Baig <aquibbaig97@gmail.com>
2019-05-31 10:26:54 +0530

    add gatsby/material-ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,18 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images/`
       }
+    },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      // If you want to use styled components, in conjunction to Material-UI, you should:
+      // - Change the injection order
+      // - Add the plugin
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      }
+      // 'gatsby-plugin-styled-components',
     }
   ]
 };
