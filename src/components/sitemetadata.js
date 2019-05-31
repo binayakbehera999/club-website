af737e28a7887939f5f566c9fbd39466eded23ef
Aquib Baig <aquibbaig97@gmail.com>
2019-05-31 10:35:27 +0530

    remove unnecessary CDNs

--- src/components/sitemetadata.js
+++ src/components/sitemetadata.js
@@ -20,20 +20,6 @@ const Sitemetadata = () => {
         name="viewport"
         content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
       />
-      <script
-        src="https://unpkg.com/react/umd/react.production.js"
-        crossorigin
-      />
-
-      <script
-        src="https://unpkg.com/react-dom/umd/react-dom.production.js"
-        crossorigin
-      />
-
-      <script
-        src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-        crossorigin
-      />
 
       <script>var Alert = ReactBootstrap.Alert;</script>
     </Helmet>
